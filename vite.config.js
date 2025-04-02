e45eac385fe07c9eca11a663f6dcedcad39cd0d5
tonyStark3124 <tuviahefets@gmail.com>
2025-04-03 00:36:04 +0300

    תיאור העדכון

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      jpg: {
+        quality: 80, // איכות מופחתת לתמונות JPG
+      },
+      png: {
+        quality: 80,
+      },
+      webp: {
+        quality: 80,
+        lossless: false,
+      },
+    }),
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000, // העלאת גבול האזהרה ל-1MB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          lottie: ['@lottiefiles/react-lottie-player'],
+          react: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
+});
